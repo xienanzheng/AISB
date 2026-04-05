37016bc22c4b117060a354b4bda5ac00f7f4c4cc
Bary Levy <barylevi@gmail.com>
2026-04-05 17:45:39 +0300

    rename exercise 2 solution. polish exercise 1

--- day4-training-and-data/day4_ex2_solution.py
+++ day4-training-and-data/day4_ex2_solution.py
@@ -6,21 +6,6 @@
 
 """
 
-
-"""
-# Exercise - Model Editing
-
-> **Difficulty**: 🔴⚪⚪⚪⚪
-> **Importance**: 🔵🔵🔵🔵⚪
->
-> You should spend up to ~60 minutes on this exercise.
-
-1. read https://blog.mithrilsecurity.io/poisongpt-how-we-hid-a-lobotomized-llm-on-hugging-face-to-spread-fake-news/
-2. play with and understand https://colab.research.google.com/github/kmeng01/rome/blob/main/notebooks/rome.ipynb
-3. pick a "fake news" and add it to the model
-4. Solution: https://colab.research.google.com/drive/16RPph6SobDLhisNzA5azcP-0uMGGq10R?usp=sharing&ref=blog.mithrilsecurity.io
-"""
-
 # %%
 """
 ## Exercise 2: Backdoor Attack via Instruction Tuning Poisoning
